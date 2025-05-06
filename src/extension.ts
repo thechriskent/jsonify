640c249f5d257455e92085ba51b491a29d65ed0c
Chris Kent <8364109+thechriskent@users.noreply.github.com>
2025-05-06 14:41:48 -0700

    Grammar Loading Error Handling

--- src/extension.ts
+++ src/extension.ts
@@ -31,9 +31,14 @@ export async function activate(context: vscode.ExtensionContext) {
 		loadGrammar: () => Promise.resolve(vsctm.parseRawGrammar(grammarContent, grammarPath))
 	});
 
-	registry.loadGrammar('source.horsescript').then((loadedGrammar) => {
-		grammar = loadedGrammar;
-	});
+	registry.loadGrammar('source.horsescript')
+		.then((loadedGrammar) => {
+			grammar = loadedGrammar;
+		})
+		.catch((error) => {
+			vscode.window.showErrorMessage('Failed to load grammar: ' + error);
+			console.error('Failed to load grammar:', error);
+		});
 
 	const outputChannel = vscode.window.createOutputChannel('JSONify');
 
@@ -281,12 +286,12 @@ export async function activate(context: vscode.ExtensionContext) {
 		],
 		{
 			provideHover(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken) {
-				// const range = document.getWordRangeAtPosition(position, /@?\w+\b/);
-				// const word = document.getText(range);
+				//const range = document.getWordRangeAtPosition(position, /@?\w+\b/);
+				//const word = document.getText(range);
 
 				// document.
 
-				// console.log('Hovering over: ' + word);
+				//console.log('Hovering over: ' + word);
 				// const hover = new vscode.Hover('This is a hover for: ' + word);
 				// hover.range = range;
 				// return hover;
@@ -301,7 +306,7 @@ export async function activate(context: vscode.ExtensionContext) {
 					if (token.startIndex <= position.character && token.endIndex >= position.character) {
 						for( const scope of token.scopes) {
 							const hoverCard = getHoverCardForScope(scope);
-							console.log(token, hoverCard);
+							//console.log(token, hoverCard);
 							if (hoverCard) {
 								const hover = new vscode.Hover(hoverCard);
 								hover.range = new vscode.Range(position.line, token.startIndex, position.line, token.endIndex);
@@ -329,7 +334,7 @@ export async function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(completionItemTriggerProvider);
 
 	context.subscriptions.push(signatureHelpProvider);
-	//context.subscriptions.push(hoverProvider);
+	context.subscriptions.push(hoverProvider);
 }
 
 export function deactivate() { }
